39204839d8867981780462a314240a071eec5049
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-08-29 18:31:56 -0300

    Adição de padding no texto

--- unidade01/pathway02/ConviteAniversario/app/src/main/java/com/example/conviteaniversario/MainActivity.kt
+++ unidade01/pathway02/ConviteAniversario/app/src/main/java/com/example/conviteaniversario/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.conviteaniversario.ui.theme.ConviteDeAniversárioTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +40,7 @@ fun Greeting(
     Surface(color = Color.Green) {
         Text(
             text = "Oi, meu nome é $name!",
-            modifier = modifier
+            modifier = modifier.padding(24.dp)
         )
     }
 }
