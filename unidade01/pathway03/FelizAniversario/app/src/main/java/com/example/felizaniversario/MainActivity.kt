531225f0edb07d6f170de827faefd648b2c842f7
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-09-01 13:51:00 -0300

    Organização dos elementos em coluna

--- unidade01/pathway03/FelizAniversario/app/src/main/java/com/example/felizaniversario/MainActivity.kt
+++ unidade01/pathway03/FelizAniversario/app/src/main/java/com/example/felizaniversario/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,14 +32,25 @@ class MainActivity : ComponentActivity() {
 // Função combinável que descreve elemento Text
 @Composable
 fun GreetingText(
-    mensagem: String,
+    mensagem: String,   // mensagem a ser exibida na tela
+    remetente: String,  // nome da pessoa que enviou o cartão
     modifier: Modifier = Modifier
 ) {
-    Text(
-        text = mensagem,     // mensagem exibida na tela
-        fontSize = 100.sp,   // tamanho da fonte
-        lineHeight = 116.sp, // altura da linha
-    )
+    Column( // exibir elementos verticalmente
+        modifier = modifier // transmitir parâmetro modificador aos elementos filhos
+    ) {
+        // Primeiro texto
+        Text(
+            text = mensagem,        // mensagem exibida na tela
+            fontSize = 30.sp,       // tamanho da fonte
+            lineHeight = 116.sp,    // altura da linha
+        )
+        // Segundo texto
+        Text(
+            text = "- de $remetente",   // mensagem exibida na tela
+            fontSize = 20.sp            // tamanho da fonte
+        )
+    }
 }
 
 // Função combinável de visualização (preview)
@@ -46,6 +58,9 @@ fun GreetingText(
 @Composable
 fun PreviaCartaoDeAniversario() {
     FelizAniversárioTheme {
-    GreetingText("Feliz Aniversário, Sam!")
+    GreetingText(
+        mensagem  = "Feliz Aniversário, Sam!",
+        remetente = "Alexandre"
+    )
     }
 }
