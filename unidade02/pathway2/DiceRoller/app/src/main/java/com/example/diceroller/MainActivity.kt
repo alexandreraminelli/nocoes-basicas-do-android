1287ed4ff7e6a8a45e42d2b7c187d8b58abed0e8
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-08 15:23:47 -0300

    remoção do conteúdo pré-definido

--- unidade02/pathway2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ unidade02/pathway2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -20,28 +20,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceRollerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   
                 }
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DiceRollerApp(){
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DiceRollerTheme {
-        Greeting("Android")
-    }
+fun DiceWithButtonAndImage(){
+
 }
