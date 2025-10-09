37576b48b8e9aaa884719f813b4d4c8b2be4f9b6
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-09 10:31:38 -0300

    Adicionar imagem do dado

--- unidade02/pathway2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ unidade02/pathway2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
@@ -14,8 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,6 +48,14 @@ fun DiceRollerApp() {
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        // imagem do dado
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "Dado exibindo o número 1"
+        )
+        // espaçamento entre imagem e botão
+        Spacer(modifier = Modifier.height(16.dp))
+        // botão de rolar dados
         Button(onClick = {
             // TODO: sortear número do dado
         }) {
