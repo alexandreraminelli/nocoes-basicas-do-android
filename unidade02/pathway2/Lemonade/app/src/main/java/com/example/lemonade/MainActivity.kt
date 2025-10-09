248c79017839a7111e37daa8467e6a84358c5c05
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-09 11:32:16 -0300

    adicionar variáveis de estado

--- unidade02/pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ unidade02/pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,10 +38,43 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun LemonadeApp(modifier: Modifier = Modifier) {
+    /** Etapa do processo. */
+    var step by remember { mutableStateOf(1) }
+
+    /** Texto de instruções da etapa. */
+    val instruction = when (step) {
+        1 -> R.string.first_step_instructions
+        2 -> R.string.second_step_instructions
+        3 -> R.string.third_step_instructions
+        4 -> R.string.fourth_step_instructions
+        else -> "Error"
+    }
+
+    /** Imagem da etapa. */
+    val image = when (step) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        4 -> R.drawable.lemon_restart
+        else -> R.drawable.ic_launcher_background
+    }
+
+    /** Descrição da imagem da etapa. */
+    val imgDesc = when (step) {
+        1 -> R.string.first_step_img
+        2 -> R.string.second_step_img
+        3 -> R.string.third_step_img
+        4 -> R.string.fourth_step_img
+        else -> "Error"
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) { Text("Limonada") }
+    ) {
+
+
+    }
 }
