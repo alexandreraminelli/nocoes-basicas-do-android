8f934602fd32469fcc503c230d5158ff63869bb6
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-12 18:16:08 -0300

    definir aparência e comportamento do EditNumberField

--- unidade02/pathway3/basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ unidade02/pathway3/basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -41,6 +42,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
@@ -111,6 +113,10 @@ fun EditNumberField(modifier: Modifier = Modifier) {
 
     TextField(
         value = amountInput, // valor do campo
+        label = { Text(stringResource(R.string.bill_amount)) }, // Rótulo
+        singleLine = true, // apenas uma linha de texto rolavel horizontalmente
+        // Tipo de teclado: numérico
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         onValueChange = { // Lambda executada quando o valor do campo mudar
             amountInput = it
         },
