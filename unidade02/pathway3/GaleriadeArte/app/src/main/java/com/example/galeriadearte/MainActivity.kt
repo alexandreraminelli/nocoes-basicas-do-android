f5fbedebcd5aa6104d5140a1688f16de5681c162
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-13 19:05:09 -0300

    botões de controle da exibição

--- unidade02/pathway3/GaleriadeArte/app/src/main/java/com/example/galeriadearte/MainActivity.kt
+++ unidade02/pathway3/GaleriadeArte/app/src/main/java/com/example/galeriadearte/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.galeriadearte.ui.theme.GaleriaDeArteTheme
@@ -35,10 +41,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtSpaceLayout(
     modifier: Modifier = Modifier
-
 ) {
     Column(
-        modifier = Modifier.padding(horizontal = 16.dp, vertical = 28.dp)
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 28.dp)
+            .fillMaxHeight()
     ) {
         // Parede de obras de arte
         ArtworkWall()
@@ -72,7 +81,44 @@ fun ArtworkDescription(
 fun DisplayController(
     modifier: Modifier = Modifier
 ) {
-    Text("Controlador de exibição")
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp), // espaçamento entre os botões
+        modifier = Modifier,
+    ) {
+        val buttonModifier = Modifier.weight(1f)
+
+        // Botão de voltar
+        ControllerButton(
+            label = stringResource(R.string.previous),
+            onClick = {
+                // TODO: imagem anterior
+            },
+            modifier = buttonModifier,
+        )
+        // Botão de avançar
+        ControllerButton(
+            label = stringResource(R.string.next),
+            onClick = {
+                // TODO: próxima imagem
+            },
+            modifier = buttonModifier,
+        )
+    }
+}
+
+/** Botão do controlador. */
+@Composable
+fun ControllerButton(
+    onClick: () -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+    ) {
+        Text(label)
+    }
 }
 
 @Preview(showBackground = true)
