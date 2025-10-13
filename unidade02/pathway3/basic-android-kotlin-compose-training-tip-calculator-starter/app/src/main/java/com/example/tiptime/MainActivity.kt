c3840eb3ced17ddd50e2913947e7f53abb57cf0d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-13 11:32:21 -0300

    ícones nos campos de texto

--- unidade02/pathway3/basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ unidade02/pathway3/basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -35,19 +36,20 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -112,6 +114,7 @@ fun TipTimeLayout() {
         )
         EditNumberField( // Valor da conta
             label = R.string.bill_amount,
+            leadingIcon = R.drawable.money,
             value = amountInput,
             onValueChange = { amountInput = it },
             keyboardOptions = KeyboardOptions.Default.copy(
@@ -124,6 +127,7 @@ fun TipTimeLayout() {
         )
         EditNumberField( // Porcentagem da gorjeta
             label = R.string.how_was_the_service,
+            leadingIcon = R.drawable.percent,
             value = tipInput,
             onValueChange = { tipInput = it },
             keyboardOptions = KeyboardOptions.Default.copy(
@@ -162,7 +166,7 @@ private fun calculateTip(
     roundUp: Boolean
 ): String {
     var tip = tipPercent / 100 * amount
-    if(roundUp) { // Arredondar se solicitado
+    if (roundUp) { // Arredondar se solicitado
         tip = kotlin.math.ceil(tip)
     }
     return NumberFormat.getCurrencyInstance().format(tip)
@@ -198,6 +202,7 @@ fun RoundTheTipRow(
 @Composable
 fun EditNumberField(
     @StringRes label: Int, // referência de recurso de String
+    @DrawableRes leadingIcon: Int,
     value: String,
     onValueChange: (String) -> Unit,
     keyboardOptions: KeyboardOptions,
@@ -208,6 +213,12 @@ fun EditNumberField(
         label = { Text(stringResource(label)) }, // rótulo
         singleLine = true, // apenas uma linha de texto scrollable horizontalmente
         // Tipo de teclado: numérico
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = leadingIcon),
+                null
+            )
+        },
         keyboardOptions = keyboardOptions,
         onValueChange = onValueChange,
         modifier = modifier
