a148d97205b7b62ecc70f8d5419a8577f0650a8d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-22 11:27:47 -0300

    grade de cursos

--- unidade02/pathway3/Courses/app/src/main/java/com/example/courses/MainActivity.kt
+++ unidade02/pathway3/Courses/app/src/main/java/com/example/courses/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -22,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.courses.data.DataSource
 import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
 
@@ -32,7 +36,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoursesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CoursesApp()
+                    CoursesApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -40,14 +46,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CoursesApp() {
-    CoursesGrid()
+fun CoursesApp(modifier: Modifier = Modifier) {
+    CoursesGrid(modifier = modifier)
 }
 
 /** Grade de cursos. */
 @Composable
 fun CoursesGrid(modifier: Modifier = Modifier) {
-    Text("Grade de cursos")
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+            .padding(8.dp)
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
+        }
+    }
 }
 
 @Preview
