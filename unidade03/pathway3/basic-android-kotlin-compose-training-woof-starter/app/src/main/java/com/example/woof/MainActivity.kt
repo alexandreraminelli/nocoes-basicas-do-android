e5ae09ea6fb6e231708babcc9a018c4edd6b7a61
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-22 18:03:47 -0300

    expandir/recolher card para mostrar hobbies dos cachorros

--- unidade03/pathway3/basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/MainActivity.kt
+++ unidade03/pathway3/basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/MainActivity.kt
@@ -32,6 +32,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -136,7 +137,7 @@ fun DogItem(
     var expanded by remember { mutableStateOf(false) }
 
     Card(modifier = modifier) {
-        Column() {
+        Column {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -148,20 +149,23 @@ fun DogItem(
                 DogItemButton(  // Botão que abre/fecha a lista
                     expanded = expanded,
                     onClick = {
-                        // TODO: exibir/ocultar informações adicionais
+                        expanded = !expanded
                     }
                 )
             }
-            // Informações adicionais (ocultas por padrão)
-            DogHobby(
-                dog.hobbies,
-                modifier = Modifier.padding(
-                    start = dimensionResource(R.dimen.padding_medium),
-                    top = dimensionResource(R.dimen.padding_small),
-                    end = dimensionResource(R.dimen.padding_medium),
-                    bottom = dimensionResource(R.dimen.padding_medium)
+            if (expanded) {
+                // Informações adicionais (ocultas por padrão)
+                DogHobby(
+                    dog.hobbies,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_small),
+                        end = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_medium)
+                    )
                 )
-            )
+            }
+
         }
     }
 }
@@ -178,7 +182,8 @@ private fun DogItemButton(
         modifier = modifier
     ) {
         Icon(
-            imageVector = Icons.Filled.ExpandMore,
+            // atualizar ícone dependendo do estado do card (expandido ou não)
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
             contentDescription = stringResource(R.string.expand_button_content_description),
             tint = MaterialTheme.colorScheme.secondary
         )
