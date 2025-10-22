017e6100e089571aeb488f47416a9f873544c25d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-22 16:43:40 -0300

    definir formato dos cards

--- unidade03/pathway3/basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/ui/theme/Shape.kt
+++ unidade03/pathway3/basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/ui/theme/Shape.kt
@@ -20,5 +20,6 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(50.dp),
+    small = RoundedCornerShape(50.dp), // círculo arredondado para fotos
+    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp), // forma dos cards
 )
