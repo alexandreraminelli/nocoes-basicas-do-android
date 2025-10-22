65ecb364f5e1528eee74483bb793005112b7621f
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-22 10:55:08 -0300

    criar composable do card de tópico

--- unidade02/pathway3/Courses/app/src/main/java/com/example/courses/MainActivity.kt
+++ unidade02/pathway3/Courses/app/src/main/java/com/example/courses/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +50,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+/** Card de tópico de curso. */
 @Composable
-fun GreetingPreview() {
-    CoursesTheme {
-        Greeting("Android")
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row {
+            // Imagem
+            Image(
+                painterResource(topic.imageResourceId),
+                contentDescription = stringResource(topic.nameResourceId),
+                modifier = Modifier
+                    .size(68.dp)
+            )
+            // Informações
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier
+                    .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+            ) {
+                // Nome do tópico
+                Text(
+                    text = stringResource(topic.nameResourceId),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                // Quantidade
+                Text(
+                    text = "${topic.quant}",
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
+        }
     }
+}
+
+/** Pré-visualização do card de tópico. */
+@Preview
+@Composable
+fun TopicCardPreview() {
+    TopicCard(
+        Topic(R.string.photography, 321, R.drawable.photography)
+    )
 }
