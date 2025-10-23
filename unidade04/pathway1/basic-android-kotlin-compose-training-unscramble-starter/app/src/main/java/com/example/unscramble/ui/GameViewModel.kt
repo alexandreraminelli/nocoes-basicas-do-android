9842238d4f76b619ba925e68663bfb5d2f2235c1
Alexandre Raminelli <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-23 14:23:49 -0300

    Exibir palavra embaralhada aleatória

--- unidade04/pathway1/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ unidade04/pathway1/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -8,5 +9,41 @@ import kotlinx.coroutines.flow.asStateFlow
 class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
-    
+
+    /** Palavra emparalhada atual. */
+    private lateinit var currentWord: String
+
+    /** Palavras já utilizadas. */
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    /** Método para embaralhar a palavra atual. */
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    /** Método para escolher uma palavra aleatória e emparalhá-la. */
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    /** Função para inicializar o jogo. */
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    init {
+        resetGame()
+    }
 }
